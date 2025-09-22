7e6f3739c5ecc936bab8a80f0c00f968b49356c2
mansigupta-4 <mansigupta8080@gmail.com>
2025-09-22 18:56:39 +0530

    add

---
+++ module/index.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const indexschema = new Schema ({
+    title : {
+        type : String,
+        required : true ,
+    },
+    author : String,
+    description : String,
+    image : {
+        url: String,
+        filename:String,
+    },
+    year : String,
+  
+})
+const Index = mongoose.model("Index",indexschema );
+module.exports=Index;
